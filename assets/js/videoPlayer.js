239aaf33a77e977605c4291ea821a01ace6ae5b1
Ilya Afanasev <chylmanprosto@gmail.com>
2022-05-26 17:59:19 +0300

    Добаляет превью видео и спинер

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -5,18 +5,34 @@ const initPlayer = () => {
     }
     
     const localPlayer = document.querySelector('[data-video-player]');
+    const previewVideoPlayer = document.querySelector('[data-video-preview]');
     const playButton = document.querySelector('.intro__video-button');
+    const spinner = document.querySelector('[data-video-spinner]');
  
     playButton.classList.add('hidden');
     videoWrap.classList.add('active');
+
+    if (!previewVideoPlayer || !localPlayer) {
+        return;
+    }
     
-    localPlayer.addEventListener('click', () => {
-        if (localPlayer.muted) {
+    previewVideoPlayer.addEventListener('click', (evt) => {
+        if (localPlayer.readyState === 3 || localPlayer.readyState === 4) {
+            previewVideoPlayer.classList.add('hidden');
+            previewVideoPlayer.pause();
             localPlayer.muted = false;
             localPlayer.setAttribute('controls', '');
-            setTimeout(() => {
+            localPlayer.play();
+        } else {
+            spinner.classList.remove('hidden');
+            localPlayer.addEventListener('canplay', () =>  {
+                spinner.classList.add('hidden');
+                previewVideoPlayer.classList.add('hidden');
+                previewVideoPlayer.pause();
+                localPlayer.muted = false;
+                localPlayer.setAttribute('controls', '');
                 localPlayer.play();
-            }, 1); 
+            })
         }
     });    
 }
