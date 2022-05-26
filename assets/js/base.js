6f5d92b59bf01d325a9a0cd56af4e4a44605661a
Ilya Afanasev <chylmanprosto@gmail.com>
2022-05-26 17:27:05 +0300

    Фикс меню

--- assets/js/base.js
+++ assets/js/base.js
@@ -70,183 +70,181 @@ function getCookie(name) {
       }
     })()
 
-    class Accordions {
-        constructor() {
-          this._openHeight = 0;
-          this._windowWidth = window.innerWidth;
-          this._documentClickHandler = this._documentClickHandler.bind(this);
-          this._windowResizeHandler = this._windowResizeHandler.bind(this);
-          this._init();
-        }
-
-        _init() {
-          this.fullUpdate();
-          document.addEventListener('click', this._documentClickHandler);
-          window.addEventListener('resize', this._windowResizeHandler);
-
-          const contents = document.querySelectorAll('[data-accordion="content"]');
-          contents.forEach(element => {
-              const links = element.querySelectorAll('a');
-              
-              links.forEach(link => {
-                  link.setAttribute('tabindex', '-1');
+        class Accordions {
+            constructor() {
+              this._openHeight = 0;
+              this._windowWidth = window.innerWidth;
+              this._documentClickHandler = this._documentClickHandler.bind(this);
+              this._windowResizeHandler = this._windowResizeHandler.bind(this);
+              this._init();
+            }
+    
+            _init() {
+              this.fullUpdate();
+              document.addEventListener('click', this._documentClickHandler);
+              window.addEventListener('resize', this._windowResizeHandler);
+    
+              const contents = document.querySelectorAll('[data-accordion="content"]');
+              contents.forEach(element => {
+                  const links = element.querySelectorAll('a');
+                  
+                  links.forEach(link => {
+                      link.setAttribute('tabindex', '-1');
+                  });
               });
-          });
-        }
-
-        _documentClickHandler(evt) {
-          const target = evt.target;
-          if (!target.closest('[data-accordion="button"]')) {
-            return;
-          }
-
-          evt.preventDefault();
-          const element = target.closest('[data-accordion="element"]');
-          if (element.classList.contains('is-active')) {
-            this.closeAccordion(element);
-            return;
-          }
-          this.openAccordion(element);
-        }
-
-        _windowResizeHandler() {
-          if (this._windowWidth === window.innerWidth) {
-            return;
-          }
-          this._windowWidth = window.innerWidth;
-          this.updateAccordionsHeight();
-        }
-
-        closeAllAccordion(parent) {
-          const elements = parent.querySelectorAll('[data-accordion="element"]');
-          elements.forEach((element) => {
-            const currentParent = element.closest('[data-accordion="parent"]');
-            if (currentParent === parent) {
-              this.closeAccordion(element);
             }
-          });
-        }
-
-        updateAccordionsHeight(element = null) {
-          if (element) {
-            const content = element.querySelector('[data-accordion="content"]');
-            content.style.transition = 'none';
-            content.style.maxHeight = `${content.scrollHeight}px`;
-            setTimeout(() => {
-              content.style.transition = null;
-            });
-            return;
-          }
-          const openElements = document.querySelectorAll('[data-accordion="element"].is-active');
-          openElements.forEach((openElement) => {
-            const content = openElement.querySelector('[data-accordion="content"]');
-            content.style.transition = 'none';
-            content.style.maxHeight = `${content.scrollHeight}px`;
-            setTimeout(() => {
-              content.style.transition = null;
-            });
-          });
-        }
-
-        fullUpdate(parent = null, transition = false) {
-          let openElements;
-          if (parent) {
-            openElements = parent.querySelectorAll('[data-accordion="element"].is-active');
-          } else {
-            openElements = document.querySelectorAll('[data-accordion="element"].is-active');
-          }
-          openElements.forEach((openElement) => {
-            const innerParent = openElement.querySelector('[data-accordion="parent"]');
-            if (innerParent) {
-              return;
+    
+            _documentClickHandler(evt) {
+              const target = evt.target;
+              if (!target.closest('[data-accordion="button"]')) {
+                return;
+              }
+    
+              evt.preventDefault();
+              const element = target.closest('[data-accordion="element"]');
+              if (element.classList.contains('is-active')) {
+                this.closeAccordion(element);
+                return;
+              }
+              this.openAccordion(element);
             }
-            this.openAccordion(openElement, transition);
-          });
-        }
-
-        openAccordion(element, transition = true) {
-          const parentElement = element.closest('[data-accordion="parent"]');
-          const contentElement = element.querySelector('[data-accordion="content"]');
-          const contentElementLinks = contentElement.querySelectorAll('a');
-
-          
-          this._openHeight += contentElement.scrollHeight;
-          
-          if (parentElement.hasAttribute('data-single')) {
-              this.closeAllAccordion(parentElement);
+    
+            _windowResizeHandler() {
+              if (this._windowWidth === window.innerWidth) {
+                return;
+              }
+              this._windowWidth = window.innerWidth;
+              this.updateAccordionsHeight();
             }
+    
+            closeAllAccordion(parent) {
+              const elements = parent.querySelectorAll('[data-accordion="element"]');
+              elements.forEach((element) => {
+                const currentParent = element.closest('[data-accordion="parent"]');
+                if (currentParent === parent) {
+                  this.closeAccordion(element);
+                }
+              });
+            }
+    
+            updateAccordionsHeight(element = null) {
+              if (element) {
+                const content = element.querySelector('[data-accordion="content"]');
+                content.style.transition = 'none';
+                content.style.maxHeight = `${content.scrollHeight}px`;
+                setTimeout(() => {
+                  content.style.transition = null;
+                });
+                return;
+              }
+              const openElements = document.querySelectorAll('[data-accordion="element"].is-active');
+              openElements.forEach((openElement) => {
+                const content = openElement.querySelector('[data-accordion="content"]');
+                content.style.transition = 'none';
+                content.style.maxHeight = `${content.scrollHeight}px`;
+                setTimeout(() => {
+                  content.style.transition = null;
+                });
+              });
+            }
+    
+            fullUpdate(parent = null, transition = false) {
+              let openElements;
+              if (parent) {
+                openElements = parent.querySelectorAll('[data-accordion="element"].is-active');
+              } else {
+                openElements = document.querySelectorAll('[data-accordion="element"].is-active');
+              }
+              openElements.forEach((openElement) => {
+                const innerParent = openElement.querySelector('[data-accordion="parent"]');
+                if (innerParent) {
+                  return;
+                }
+                this.openAccordion(openElement, transition);
+              });
+            }
+    
+            openAccordion(element, transition = true) {
+              const parentElement = element.closest('[data-accordion="parent"]');
+              const contentElement = element.querySelector('[data-accordion="content"]');
+              const contentElementLinks = contentElement.querySelectorAll('a');
+    
+              
+              this._openHeight += contentElement.scrollHeight;
+              
+              if (parentElement.hasAttribute('data-single')) {
+                  this.closeAllAccordion(parentElement);
+                }
+                
+                contentElementLinks.forEach(element => {
+                  element.removeAttribute('tabindex');
+                });
+              element.classList.add('is-active');
+              if (transition) {
+                contentElement.style.maxHeight = `${this._openHeight}px`;
+              } else {
+                contentElement.style.transition = 'none';
+                contentElement.style.maxHeight = `${this._openHeight}px`;
+                setTimeout(() => {
+                  contentElement.style.transition = null;
+                });
+              }
+    
+              if (parentElement.closest('[data-accordion="element"]')) {
+                this.openAccordion(parentElement.closest('[data-accordion="element"]'), transition);
+                return;
+              }
+    
+              this._openHeight = 0;
+            }
+    
+            closeAccordion(element, transition = true) {
+              const contentElement = element.querySelector('[data-accordion="content"]');
+              
+              
+              if (!contentElement) {
+                  return;
+              }
+              
+              element.classList.remove('is-active');
+              
+              if (transition) {
+                contentElement.style.maxHeight = '0';
+              } else {
+                contentElement.style.transition = 'none';
+                contentElement.style.maxHeight = '0';
+                setTimeout(() => {
+                  contentElement.style.transition = null;
+                });
+              }
+            }
+        }
+        
+          document.addEventListener("DOMContentLoaded", () => {
+            const maxWidthAccordions = document.querySelector('[data-accordion = "parent"]').dataset.accordionMaxWidth;
+            let clientWidth = document.documentElement.clientWidth;
             
-            contentElementLinks.forEach(element => {
-              element.removeAttribute('tabindex');
-            });
-          element.classList.add('is-active');
-          if (transition) {
-            contentElement.style.maxHeight = `${this._openHeight}px`;
-          } else {
-            contentElement.style.transition = 'none';
-            contentElement.style.maxHeight = `${this._openHeight}px`;
+            const mediaQuery = window.matchMedia(`(max-width: ${maxWidthAccordions}px)`);
+            const breakpoint = window.matchMedia(`(max-width:1024px)`);
+    
+            let accordions;
+            const initAccordions = () => {
+                accordions = new Accordions();
+                // Используйте в разработке экспортируемую переменную accordions, window сделан для бэкэнда
+                window.accordions = accordions;
+            };
+  
+            const breakpointChecker = () => {
+            if (breakpoint.matches) {
+              initAccordions();
+            } 
+            };
+            
             setTimeout(() => {
-              contentElement.style.transition = null;
-            });
-          }
-
-          if (parentElement.closest('[data-accordion="element"]')) {
-            this.openAccordion(parentElement.closest('[data-accordion="element"]'), transition);
-            return;
-          }
-
-          this._openHeight = 0;
-        }
-
-        closeAccordion(element, transition = true) {
-          const contentElement = element.querySelector('[data-accordion="content"]');
-          
-          
-          if (!contentElement) {
-              return;
-          }
-          
-          element.classList.remove('is-active');
-          
-          if (transition) {
-            contentElement.style.maxHeight = '0';
-          } else {
-            contentElement.style.transition = 'none';
-            contentElement.style.maxHeight = '0';
-            setTimeout(() => {
-              contentElement.style.transition = null;
-            });
-          }
-        }
-    }
-
-    
-    
-    let accordions;
-
-    const initAccordions = () => {
-        accordions = new Accordions();
-        // Используйте в разработке экспортируемую переменную accordions, window сделан для бэкэнда
-        window.accordions = accordions;
-    };
-
-    const maxWidthAccordions = document.querySelector('[data-accordion = "parent"]').dataset.accordionMaxWidth;
-    let clientWidth = document.documentElement.clientWidth;
-    
-    const mediaQuery = window.matchMedia(`(max-width: ${maxWidthAccordions}px)`);
-    const breakpoint = window.matchMedia(`(max-width:1024px)`);
-    
-    const breakpointChecker = () => {
-      if (breakpoint.matches) {
-        initAccordions();
-      } else {
-      }
-    };
-
-    document.addEventListener("DOMContentLoaded", () => {
-      breakpoint.addListener(breakpointChecker);
-      breakpointChecker();
-    });
+              breakpoint.addListener(breakpointChecker);
+              breakpointChecker();
+            }, 500);
+          });          
     class ScrollLock {
         constructor() {
           this._iosChecker = iosChecker;
