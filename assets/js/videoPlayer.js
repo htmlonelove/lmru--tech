f85c77227c8a7efa171eee180046e5bf6055ad9b
Ilya Afanasev <chylmanprosto@gmail.com>
2022-05-30 19:45:38 +0300

    Возвращает первую версию видео

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -5,54 +5,40 @@ const initPlayer = () => {
     }
     
     const localPlayer = document.querySelector('[data-video-player]');
-    const previewVideoPlayer = document.querySelector('[data-video-preview]');
-    const spinner = document.querySelector('[data-video-spinner]');
- 
-    // playButton.classList.add('hidden');
-    // videoWrap.classList.add('active');
 
-    if (!previewVideoPlayer || !localPlayer) {
+    if (!localPlayer) {
         return;
     }
-
     
     window.addEventListener('load', () => {
-        const previewSrc = previewVideoPlayer.dataset.src;
         const localSrc = localPlayer.dataset.src;
-
-        previewVideoPlayer.classList.add('hidden');
         localPlayer.classList.add('hidden');
-        previewVideoPlayer.src = previewSrc;
         localPlayer.src = localSrc;
-
-        previewVideoPlayer.addEventListener('canplay', ()=> {
+        if (localPlayer.readyState === 3 || localPlayer.readyState === 4) {
             videoWrap.classList.add('active');
-            previewVideoPlayer.classList.remove('hidden');
-        })
-    
-        previewVideoPlayer.addEventListener('click', (evt) => {
-            if (localPlayer.readyState === 3 || localPlayer.readyState === 4) {
-                previewVideoPlayer.classList.add('hidden');
-                previewVideoPlayer.pause();
-                localPlayer.classList.remove('hidden');
+            localPlayer.classList.remove('hidden');
+            localPlayer.addEventListener('click', () => {
                 localPlayer.muted = false;
                 localPlayer.setAttribute('controls', '');
-                localPlayer.play();
-            } else {
-                spinner.classList.remove('hidden');
-                localPlayer.addEventListener('canplay', () =>  {
-                    spinner.classList.add('hidden');
-                    previewVideoPlayer.classList.add('hidden');
-                    localPlayer.classList.remove('hidden');
-                    previewVideoPlayer.pause();
+
+                setTimeout(() => {
+                    localPlayer.play();
+                }, 10);
+            }, {once: true});
+        } else {
+            localPlayer.addEventListener('canplay', () =>  {
+                videoWrap.classList.add('active');
+                localPlayer.classList.remove('hidden');
+                localPlayer.addEventListener('click', () => {
                     localPlayer.muted = false;
                     localPlayer.setAttribute('controls', '');
-                    localPlayer.play();
-                })
+                    setTimeout(() => {
+                        localPlayer.play();
+                    }, 10);
+                }, {once: true});
+            })
             }
         });  
-    });
-    
 }
 
 initPlayer();
